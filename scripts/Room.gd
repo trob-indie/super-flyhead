61ebe3c23b5be0ad5cf079d8bb7c1769ac257106
Tyler Robinson <inbox.tylerrobinson@gmail.com>
2025-07-05 10:32:07 -0400

    generate a true oval collider in Room.gd

--- scripts/Room.gd
+++ scripts/Room.gd
@@ -29,29 +29,55 @@ func create_capsule_ring_polygon(
 	var ow2 = outer_width / 2
 	var iw2 = inner_width / 2
 
-	# OUTER EDGE
+	var left_arc_top : Vector2
+	var right_arc_top : Vector2
+	var left_arc_bottom : Vector2
+	var right_arc_bottom : Vector2
+	var inner_left_bottom : Vector2
+
+	# OUTER EDGE -- left arc (top-left to bottom-left)
 	for i in range(arc_resolution + 1):
 		var angle = lerp(PI / 2, 3 * PI / 2, float(i) / arc_resolution)
-		outer.append(Vector2(-ow2, 0) + Vector2(cos(angle), sin(angle)) * outer_radius)
-	# Don't duplicate the top-right point, start from i=1
+		var pt = Vector2(-ow2, 0) + Vector2(cos(angle), sin(angle)) * outer_radius
+		if i == 0:
+			left_arc_top = pt
+		if i == arc_resolution:
+			left_arc_bottom = pt
+		outer.append(pt)
+
+	# BOTTOM RECTANGLE SEGMENT (connect bottom-left to bottom-right)
+	right_arc_bottom = Vector2(ow2, left_arc_bottom.y)
+	outer.append(right_arc_bottom)
+
+	# OUTER EDGE -- right arc (bottom-right to top-right)
 	for i in range(1, arc_resolution + 1):
 		var angle = lerp(-PI / 2, PI / 2, float(i) / arc_resolution)
-		outer.append(Vector2(ow2, 0) + Vector2(cos(angle), sin(angle)) * outer_radius)
+		var pt = Vector2(ow2, 0) + Vector2(cos(angle), sin(angle)) * outer_radius
+		if i == arc_resolution:
+			right_arc_top = pt
+		outer.append(pt)
 
-	# INNER EDGE
+	# TOP RECTANGLE SEGMENT (connect top-right to top-left)
+	outer.append(left_arc_top)
+
+	# INNER EDGE -- left arc (top-left to bottom-left, along left endcap, but inner)
 	for i in range(arc_resolution + 1):
 		var angle = lerp(PI / 2, 3 * PI / 2, float(i) / arc_resolution)
-		inner.append(Vector2(-iw2, 0) + Vector2(cos(angle), sin(angle)) * inner_radius)
-	# Don't duplicate the top-left point, start from i=1
+		var pt = Vector2(-iw2, 0) + Vector2(cos(angle), sin(angle)) * inner_radius
+		if i == 0:
+			inner_left_bottom = pt
+		inner.append(pt)
+
+	# INNER EDGE -- right arc (bottom-right to top-right, along right endcap, but inner)
 	for i in range(1, arc_resolution + 1):
 		var angle = lerp(-PI / 2, PI / 2, float(i) / arc_resolution)
 		inner.append(Vector2(iw2, 0) + Vector2(cos(angle), sin(angle)) * inner_radius)
+
+	# ADD bottom-left inner point to form bottom rectangle properly
+	inner.append(inner_left_bottom)
+
+	# Reverse so it winds correctly
 	inner.reverse()
 
 	var poly = outer + inner
-
-	# Flip Y coordinates (seam is now at top, pinched)
-	for i in range(poly.size()):
-		poly[i].y = -poly[i].y
-
 	return poly
